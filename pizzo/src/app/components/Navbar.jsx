217f49d4d472fb781c127c883080109bc91b046f
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-12 10:25:30 +0300

    created navbar

---
+++ pizzo/src/app/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Flex, Spacer,Box,Heading,ButtonGroup,Button} from '@chakra-ui/react'
+import { BsBasket } from "react-icons/bs";
+import { Text } from '@chakra-ui/react'
+
+function Navbar() {
+  return (
+    <Flex minWidth='max-content' p={2} alignItems='center' bg="gray.900" color="white" border='1px' borderColor='gray.200'  gap='2'>
+    <Box p='2'>
+      <Text size='lg' as='b' marginLeft={{base:0,md:2,lg:3}}>Pizzo</Text>
+    </Box>
+    <Spacer />
+    <ButtonGroup gap={{base:0,sm:1,md:2,lg:3}}>
+      <Button colorScheme="gray.900">Home</Button>
+      <Button colorScheme="gray.900" >Menu</Button>
+      <Button colorScheme="red" marginRight={{base:0,md:2,lg:3}}><BsBasket /></Button>
+    </ButtonGroup>
+  </Flex>
+  )
+}
+
+export default Navbar
--- pizzo/src/app/layout.jsx
+++ pizzo/src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Providers } from "./provider";
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,7 +10,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Providers>{children}</Providers>
+      <Providers>
+        <Navbar/>
+        {children}
+        </Providers>
         </body>
     </html>
   );
