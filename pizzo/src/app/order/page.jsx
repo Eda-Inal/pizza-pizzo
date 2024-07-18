7f61a36133269fc0ac9e763fc1bb611080646fa8
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-18 15:40:09 +0300

    add order infos

--- pizzo/src/app/order/page.jsx
+++ pizzo/src/app/order/page.jsx
@@ -1,19 +1,57 @@
-import { Container,Box, Text,FormControl,FormLabel,Input,Button, Center } from '@chakra-ui/react'
+'use client'
+import { Container,Box, Text,FormControl,FormLabel,Input,Button, Center,Stack,Alert,AlertIcon,CloseButton,AlertTitle,AlertDescription,onClose } from '@chakra-ui/react'
 import React from 'react'
+import { useState } from 'react'
+import Link from 'next/link'
+
 
 function Order() {
+    const [info,setInfo] = useState({name:"",date:"",cvv:"",number:""});
+    const [alert,setAlert] = useState(false);
+    const [orderAlert,setOrderAlert]=  useState(false);
+    const [close,setClose] = useState(false)
+ 
+    const handleName=(e) => {
+        setInfo({ name: e.target.value });
+      }
+      const handleDate=(e) => {
+        setInfo({ date: e.target.value });
+      }
+      const handleCvv=(e) => {
+        setInfo({ cvv: e.target.value });
+      }
+      const handleNumber=(e) => {
+        setInfo({ number: e.target.value });
+      }
+      const handleClose = () => {
+        setClose(true)
+      } 
+      const handleForm = () => {
+        if(info.name !== "" && info.date !=="" && info.cvv !== "" && info.number !==""){
+            setAlert(false);
+            setOrderAlert(true);
+
+
+        } else{
+         setAlert(true);
+        }
+      }
   return (
-   <Container color="black" centerContent  >
+    <>
+    {
+        !orderAlert && (
+<Container color="black" centerContent  >
     <Box bg="gray.200" borderRadius={10}  color="black"   p={12}>
     <FormControl isRequired>
   <FormLabel mt={2} >Name</FormLabel>
   <Input placeholder='Name'
   border={1}
   borderColor="black"
+  onChange = {handleName}
    borderStyle="solid"  />
   <FormLabel mt={2}>Expiration Date</FormLabel>
   <Input placeholder="MM/YY"
-
+  onChange = {handleDate}
   border={1}
   borderColor="black"
    borderStyle="solid"  />
@@ -22,24 +60,95 @@ function Order() {
   border={1}
   borderColor="black"
    borderStyle="solid" 
+   onChange = {handleCvv}
   />
    <FormLabel mt={2}>Card Number</FormLabel>
   <Input placeholder="1234 1234 1234 1234" 
   border={1}
   borderColor="black"
    borderStyle="solid" 
+   onChange = {handleNumber}
   />
 </FormControl>
+{
+    alert&&  ( <Stack mt={3} spacing={3} mb={3} border={1}  
+        borderColor="red.100" borderStyle="solid">
+    <Alert status='warning'>
+      <AlertIcon />
+      Please fill all fields.
+    </Alert>
+    </Stack>)
+}
 
 <Center>
-<Button mt={6} px={8} py={4} colorScheme="green">
+<Button mt={6} px={8} py={4} colorScheme="green" onClick={handleForm}>
     <Text fontSize="xl">Submit</Text>
     </Button>
 </Center>
 
     </Box>
+  
 
-   </Container>
+   </Container> 
+        )
+    }
+    
+   
+     {
+       
+             orderAlert && (
+                <> 
+             {!close &&(
+                
+                 <Alert 
+            color="black"
+            alignSelf={"flex-center"}
+            position={'relative'} 
+            mx={"auto"}
+            my={"auto"}
+           
+            width={200} status='success'>
+            <AlertIcon />
+            <Box width={100}>
+              
+              <AlertDescription>
+              Ordered!
+              </AlertDescription>
+            </Box>
+            <CloseButton
+              alignSelf='flex-start'
+              position='relative'
+              right={-1}
+              top={-1}
+              onClick={handleClose}
+            />
+          </Alert> 
+             )
+            }
+             
+             <Center>
+    <Text fontSize='xl' mt={3} mb={3}>
+Click and see the orders
+</Text>
+</Center>
+<Center>
+<Button colorScheme="red" p={5}>
+    <Link href="/profile">
+    <Text fontSize='xl'>Pizza's</Text>
+    </Link>
+</Button>
+</Center>
+</>
+
+
+          
+        )
+    }
+    
+
+    
+ 
+    </>
   )
 }
 
