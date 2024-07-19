937d0fd485ab0e71954f5f148ca107eaf9e5212f
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-19 08:28:18 +0300

    add min character condition signup inputs

--- pizzo/src/app/login/page.jsx
+++ pizzo/src/app/login/page.jsx
@@ -1,22 +1,22 @@
 'use client'
 import React from 'react'
-import { Container, Text,Box,FormControl,FormLabel,Input, Center,Button,Stack,Alert,AlertIcon} from '@chakra-ui/react'
+import { Container, Text, Box, FormControl, FormLabel, Input, Center, Button, Stack, Alert, AlertIcon } from '@chakra-ui/react'
 import Link from 'next/link'
-import { updateLogin,changeBtn, btnShow } from '../redux/pizzasSlice'
-import {useDispatch } from 'react-redux'
+import { updateLogin, changeBtn, btnShow } from '../redux/pizzasSlice'
+import { useDispatch } from 'react-redux'
 import { useState } from 'react'
 
 function Login() {
   const dispatch = useDispatch();
-  const [name,setName] = useState("");
-  const [password,setPassword] = useState("");
-  const[emptyAlert,setEmptyAlert]  =useState(false);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [emptyAlert, setEmptyAlert] = useState(false);
   const [controlPassword, setControlPassword] = useState('');
-  const [controlName,setControlName] =  useState("");
-  const [nameError,setNameError] = useState("");
+  const [controlName, setControlName] = useState("");
+  const [nameError, setNameError] = useState("");
   const [error, setError] = useState('');
 
-  const handleName=(e) => {
+  const handleName = (e) => {
     setName(e.target.value)
     const value = e.target.value;
     setControlName(value);
@@ -26,7 +26,7 @@ function Login() {
       setNameError('');
     }
   }
-  const handlePassword=(e) => {
+  const handlePassword = (e) => {
     setPassword(e.target.value);
     const value = e.target.value;
     setControlPassword(value);
@@ -36,98 +36,80 @@ function Login() {
       setError('');
     }
   }
-const handleForm = () => {
-  if(name !=="" && password !== ""){
-    dispatch(updateLogin({name:name,password:password}));
-    dispatch(changeBtn());
-    setEmptyAlert(false);
-    dispatch(btnShow(false))
+  const handleForm = () => {
+    if (name !== "" && password !== "") {
+      dispatch(updateLogin({ name: name, password: password }));
+      dispatch(changeBtn());
+      setEmptyAlert(false);
+      dispatch(btnShow(false))
+    }
+    else {
+      setEmptyAlert(true)
+    }
   }
-  else{
-    setEmptyAlert(true)
-  }
-
-}
-
-
   return (
-    
     <Container mt={10} centerContent>
-     
-    <Box bg="gray.100"  color="black" maxWidth={600} maxHeight={600} p={15}  borderRadius={10}>
-      <Center>
-      <FormControl  width={300} isRequired mt={10}>
-  <FormLabel >Name,Surname</FormLabel>
-  <Input border={1}
-      borderColor="black"
-       borderStyle="solid"  
-        placeholder='First name'
-        onChange = {handleName}
+      <Box bg="gray.100" color="black" maxWidth={600} maxHeight={600} p={15} borderRadius={10}>
+        <Center>
+          <FormControl width={300} isRequired mt={10}>
+            <FormLabel >Name,Surname</FormLabel>
+            <Input border={1}
+              borderColor="black"
+              borderStyle="solid"
+              placeholder='First name'
+              onChange={handleName}
 
-        />
-          { nameError && (
-            <Center>
- <Text color="red"  mb={2}>
-          {nameError}
-        </Text>
-            </Center>
-       
-      )}
+            />
+            {nameError && (
+              <Center>
+                <Text color="red" mb={2}>
+                  {nameError}
+                </Text>
+              </Center>
+            )}
+            <FormLabel mt={3}>Password</FormLabel>
+            <Input border={1}
+              borderColor="black" borderStyle="solid"
+              type="password"
+              placeholder='Password'
+              onChange={handlePassword}
+              mb={3}
+            />
+            {error && (
+              <Center>
+                <Text color="red" mb={2}>
+                  {error}
+                </Text>
+              </Center>
 
-<FormLabel mt={3}>Password</FormLabel>
-  <Input border={1}
-      borderColor="black" borderStyle="solid"
-      type = "password"
-       placeholder='Password'
-       onChange = {handlePassword}
-       
-       mb = {3}
-        />
-          {error && (
-            <Center>
- <Text color="red"  mb={2}>
-          {error}
-        </Text>
-            </Center>
-       
-      )}
-
- 
-   
-  </FormControl>
-      </Center>
-      {emptyAlert &&
-       ( <Stack spacing={3} mb={3} border={1}  
-      borderColor="red.100" borderStyle="solid">
-  <Alert status='warning'>
-    <AlertIcon />
-    Please fill all fields.
-  </Alert>
-  </Stack>)
-   }
-      <Center>
-    
-      
-      <Button  colorScheme='red' onClick={handleForm} mt={2}>  
-        <Link href={`${ name !=="" && password!=="" && !error && !nameError ? "/profile" : ""}`}>Log in </Link >
-      </Button>
-    
-      </Center>
-      <Center>
-      <Text mt={2} mb={2}>
-      You don't have account yet,sign up.
-      </Text>
-      </Center>
-      
-      <Center>
-      <Button colorScheme="green" mt={2}>
-      <Link href="/signup">Sign Up </Link >
-        </Button>
-      </Center>
-      
-    
-     
-    </Box>
+            )}
+          </FormControl>
+        </Center>
+        {emptyAlert &&
+          (<Stack spacing={3} mb={3} border={1}
+            borderColor="red.100" borderStyle="solid">
+            <Alert status='warning'>
+              <AlertIcon />
+              Please fill all fields.
+            </Alert>
+          </Stack>)
+        }
+        <Center>
+          <Button colorScheme='red' onClick={handleForm} mt={2}>
+            <Link href={`${name !== "" && password !== "" && !error && !nameError ? "/profile" : ""}`}>Log in </Link >
+          </Button>
+        </Center>
+        <Center>
+          <Text mt={2} mb={2}>
+            You don't have account yet,sign up.
+          </Text>
+        </Center>
+        <Center>
+          <Button colorScheme="green" mt={2}>
+            <Link href="/signup">Sign Up </Link >
+          </Button>
+        </Center>
+      </Box>
     </Container>
   )
 }
--- pizzo/src/app/signup/page.jsx
+++ pizzo/src/app/signup/page.jsx
@@ -1,100 +1,118 @@
 'use client'
 import React from 'react'
-import { Container, Text,Box,FormControl,FormLabel,Input, Center,Button, Stack,Alert,AlertIcon } from '@chakra-ui/react'
+import { Container, Text, Box, FormControl, FormLabel, Input, Center, Button, Stack, Alert, AlertIcon } from '@chakra-ui/react'
 import Link from 'next/link'
 import { useDispatch } from 'react-redux';
-import { updateSignup,changeBtn,btnShow } from '../redux/pizzasSlice';
+import { updateSignup, changeBtn, btnShow } from '../redux/pizzasSlice';
 import { useState } from 'react'
 
 function Signup() {
   const dispatch = useDispatch();
-  const [name,setName] = useState("");
-  const [password,setPassword] = useState("");
-  const [password2,setPassword2] = useState("");
-  const [equalAlert,setEqualAlert] = useState(false);
-  const[emptyAlert,setEmptyAlert]  =useState(false);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
+  const [equalAlert, setEqualAlert] = useState(false);
+  const [emptyAlert, setEmptyAlert] = useState(false);
+  const [controlName, setControlName] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [controlPassword, setControlPassword] = useState('');
+  const [error, setError] = useState('');
 
-  const handleName=(e) => {
+  const handleName = (e) => {
     setName(e.target.value)
- 
+    const value = e.target.value;
+    setControlName(value);
+    if (value.length < 5) {
+      setNameError('At least 5 characters');
+    } else {
+      setNameError('');
+    }
   }
-  const handlePassword=(e) => {
-    setPassword(e.target.value)
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    const value = e.target.value;
+    setControlPassword(value);
+    if (value.length < 8) {
+      setError('At least 8 characters');
+    } else {
+      setError('');
+    }
   }
   const handlePassword2 = (e) => {
     setPassword2(e.target.value)
   }
-const handleForm = () => {
-  if(password ===  password2 && name !=="" && password!=="" && password2 !=="" ){
-    dispatch(updateSignup({name:name,password:password,password2:password2}));
-    dispatch(changeBtn());
-    dispatch(btnShow(false))
-  {password === password2? setEqualAlert(false) : ""}
-  {name !=="" && password!=="" && password2 !=="" ? setEmptyAlert(false) : ""}
+  const handleForm = () => {
+    if (password === password2 && name !== "" && password !== "" && password2 !== "") {
+      dispatch(updateSignup({ name: name, password: password, password2: password2 }));
+      dispatch(changeBtn());
+      dispatch(btnShow(false))
+      { password === password2 ? setEqualAlert(false) : "" }
+      { name !== "" && password !== "" && password2 !== "" ? setEmptyAlert(false) : "" }
+    }
+    else if (password !== "" && password2 !== "" && password !== password2) {
+      setEqualAlert(true)
+      setEmptyAlert(false);
+    }
+    else if (name === "" || password === "" || password2 === "") {
+      setEmptyAlert(true);
+      setEqualAlert(false);
+    }
   }
-  else if( password!=="" && password2 !=="" &&password !== password2){
-    setEqualAlert(true)
-    setEmptyAlert(false);
-  }
-  else if(name ==="" || password==="" || password2 ===""){
-    setEmptyAlert(true);
-    setEqualAlert(false);
-    
-  }
-
-}
   return (
-  
-
     <Container mt={10} centerContent>
-    
-        
-    <Box bg="gray.100"  color="black" maxWidth={500} maxHeight={500} p={15} px={[3,6,12,16]} borderRadius={10}>
-      <Center>
-      <FormControl  width={300} isRequired mt={10}>
-  <FormLabel >Name,Surname</FormLabel>
-  <Input border={1} onChange = {handleName} 
+      <Box bg="gray.100" color="black" maxWidth={500} maxHeight={500} p={15} px={[3, 6, 8, 12]} borderRadius={10}>
+        <Center>
+          <FormControl  isRequired mt={10}>
+            <FormLabel >Name,Surname</FormLabel>
+            <Input border={1} onChange={handleName}
+              borderColor="black"
+              borderStyle="solid"
+              placeholder='First name' />
+            {nameError && (
+              <Center>
+                <Text color="red" >
+                  {nameError}
+                </Text>
+              </Center>
+            )}
+            <FormLabel mt={3}>Password</FormLabel>
+            <Input border={1} onChange={handlePassword}
+              borderColor="black" borderStyle="solid" type="password" placeholder='Password' />
+            {error && (
+              <Center>
+                <Text color="red" mb={2}>
+                  {error}
+                </Text>
+              </Center>
+            )}
+            <FormLabel mt={3}>Password</FormLabel>
+            <Input border={1} onChange={handlePassword2}
+              borderColor="black" borderStyle="solid" type="password" placeholder='Password again' />
+          </FormControl>
+        </Center>
+        {equalAlert &&
+          (<Stack spacing={3} mt={3} border={1}
+            borderColor="red.100" borderStyle="solid">
+            <Alert status='error'>
+              <AlertIcon />
+              Password does not match.
+            </Alert>
+          </Stack>)
+        }
+        {emptyAlert &&
+          (<Stack spacing={3} mt={3} border={1}
+            borderColor="red.100" borderStyle="solid">
+            <Alert status='warning'>
+              <AlertIcon />
+              Please fill all fields.
+            </Alert>
+          </Stack>)
+        }
+        <Center>
+          <Button mt={6} colorScheme='green'> <Link href={`${password === password2 && name !== "" && password !== "" && password2 !== "" && !nameError && !error ? "/profile" : ""}`} onClick={handleForm}>Sign Up </Link ></Button>
+        </Center>
 
-      borderColor="black"
-      
-        
-       borderStyle="solid"
-       placeholder='First name' />
-
-<FormLabel mt={3}>Password</FormLabel>
-  <Input border={1}  onChange = {handlePassword} 
-      borderColor="black" borderStyle="solid" type = "password"  placeholder='Password' />
-<FormLabel mt={3}>Password</FormLabel>
-  <Input border={1}  onChange = {handlePassword2} 
-      borderColor="black" borderStyle="solid" type = "password"  placeholder='Password again' />
-      
-  </FormControl>
-
-      </Center>
-      {equalAlert  &&
-       ( <Stack spacing={3} mt={3} border={1}  
-      borderColor="red.100" borderStyle="solid">
-  <Alert status='error'>
-    <AlertIcon />
-    Password does not match. 
-  </Alert>
-  </Stack>)
-   }
-    {emptyAlert &&
-       ( <Stack spacing={3} mt={3} border={1}  
-      borderColor="red.100" borderStyle="solid">
-  <Alert status='warning'>
-    <AlertIcon />
-    Please fill all fields.
-  </Alert>
-  </Stack>)
-   }
-     
-      <Center>
-      <Button mt={6} colorScheme='green'> <Link href={`${password === password2 && name !=="" && password!=="" && password2 !=="" ? "/profile" : ""}`} onClick={handleForm}>Sign Up </Link ></Button>
-      </Center>
-     
-    </Box>
+      </Box>
     </Container>
   )
 }
